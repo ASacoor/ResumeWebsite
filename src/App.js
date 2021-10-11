7dc21296ece147d8f74b1f22d0ec6da24a6342af
ASacoor <61373584+ASacoor@users.noreply.github.com>
2021-10-11 19:31:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -42,11 +42,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header data={this.state.resumeData.main} />
-        <About data={this.state.resumeData.main} />
-        <Resume data={this.state.resumeData.resume} />
+
         <Contact data={this.state.resumeData.main} />
-        <Footer data={this.state.resumeData.main} />
       </div>
     );
   }
